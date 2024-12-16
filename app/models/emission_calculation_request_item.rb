4fce77f22a9cdbba5ed739026e5393d90ac550ba
Ihcène Medjber <ihcene.medjber@silverfin.com>
2024-12-16 21:43:37 +0100

    Main computation

--- app/models/emission_calculation_request_item.rb
+++ app/models/emission_calculation_request_item.rb
@@ -1,4 +1,19 @@
 class EmissionCalculationRequestItem < ApplicationRecord
   belongs_to :emission_calculation_request
   belongs_to :emission_factor, optional: true
+
+  def compute
+    self.computed_emission_in_grams =
+      if unit == emission_factor.factor_unit
+        emission_factor.quantity_in_grames * quantity
+      else
+        emission_factor.quantity_in_grames * Unit.new("#{quantity.to_s} #{unit}").convert_to(emission_factor.factor_unit).scalar
+      end
+  rescue ArgumentError => e
+    if ['not compatible with', 'Unit not recognized'].any? { |msg| e.message.include?(msg) }
+      self.error_message = "Unit conversion error: #{e.message}"
+    else
+      raise
+    end
+  end
 end
--- app/models/emission_factor.rb
+++ app/models/emission_factor.rb
@@ -2,7 +2,14 @@ class EmissionFactor < ApplicationRecord
   def quantity_in_grames
     weight_unit, factor_unit = unit.split('/', 2)
     Unit.new("#{quantity.to_s} #{weight_unit}").convert_to('g').scalar
+    
   rescue ArgumentError => e
     raise unless ['not compatible with', 'Unit not recognized'].any? { |msg| e.message.include?(msg) }
+  end
+
+  def factor_unit
+    _, factor_unit = unit.split('/', 2)
+
+    factor_unit
   end
 end
