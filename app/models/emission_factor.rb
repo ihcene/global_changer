c329f90557f6af60a8a14ab606416f0a519b80f9
Ihcène Medjber <ihcene.medjber@silverfin.com>
2024-12-17 06:33:35 +0100

    Remove error check from emission_factor

--- app/models/emission_factor.rb
+++ app/models/emission_factor.rb
@@ -2,9 +2,6 @@ class EmissionFactor < ApplicationRecord
   def quantity_in_grames
     weight_unit, factor_unit = unit.split('/', 2)
     Unit.new("#{quantity.to_s} #{weight_unit}").convert_to('g').scalar
-    
-  rescue ArgumentError => e
-    raise unless ['not compatible with', 'Unit not recognized'].any? { |msg| e.message.include?(msg) }
   end
 
   def factor_unit
